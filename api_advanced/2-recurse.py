832346477dd704210c93c1eced94238f7cde5988
Samenergy <sdushime38@gmail.com>
2023-11-11 10:53:03 -0800

    api advanced

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,28 +1,24 @@
+#!/usr/bin/python3
+""" 2-recurse.py """
+import requests
+
+
 def recurse(subreddit, hot_list=[], after=None):
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    """ returns list with titles of all hot articles in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'MyBot/1.0'}
+    params = {"limit": 100, "after": after}
     response = requests.get(
-        url,
-        headers=headers,
-        params=params,
-        allow_redirects=False
-    )
-
-    if response.status_code != 200:
-        return None
-
-    data = response.json().get('data', {})
-
-    for child in data.get('children', []):
-        hot_list.append(child['data']['title'])
-
-    after = data.get('after', None)
-    if after is not None:
-        return recurse(subreddit, hot_list, after)
-
-    if hot_list:  # Check if hot_list is not empty
+                            url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()["data"]
+        hot_list += [post["data"]["title"] for post in data["children"]]
+        if data["after"] is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, data["after"])
+    elif response.status_code == 404:
         return "OK"
-    else:
-        return None
-
